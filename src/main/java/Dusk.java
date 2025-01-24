ab9fd2afa17a0cc28d414fefad8b0d646158f703
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-01-24 12:50:41 +0800

    refactor: refactor duke into dusk

---
+++ src/main/java/Dusk.java
@@ -0,0 +1,18 @@
+public class Dusk {
+    public static void printHorLn() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        printHorLn();
+
+        System.out.println("Hello! I'm Dusk\n" +
+                "What can I do for you?");
+
+        printHorLn();
+
+        System.out.println("Bye. Hope to see you again soon!");
+
+        printHorLn();
+    }
+}
