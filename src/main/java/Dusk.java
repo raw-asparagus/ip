774aec6afe1538ab5b52a21a9457e34d35be443a
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-01-24 13:24:22 +0800

    feat: add basic chat to dusk

--- src/main/java/Dusk.java
+++ src/main/java/Dusk.java
@@ -1,18 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Dusk {
-    public static void printHorLn() {
-        System.out.println("____________________________________________________________");
+    private static void printLine(BufferedWriter writer) throws IOException {
+        writer.write("\t____________________________________________________________\n");
     }
 
     public static void main(String[] args) {
-        printHorLn();
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))
+                ) {
+            printLine(writer);
+            writer.write("\tHello! I'm Dusk\n" +
+                    "\tAnything you want me to do for you? :D\n");
+            printLine(writer);
+            writer.flush();
 
-        System.out.println("Hello! I'm Dusk\n" +
-                "What can I do for you?");
+            String input;
+            while ((input = reader.readLine()) != null && !input.equals("bye")) {
+                printLine(writer);
+                writer.write("\t " + input + "\n");
+                printLine(writer);
+                writer.flush();
+            }
 
-        printHorLn();
+            printLine(writer);
+            writer.write("\tSee ya! Hope to see you again soon! :3\n");
+            printLine(writer);
+            writer.flush();
 
-        System.out.println("Bye. Hope to see you again soon!");
-
-        printHorLn();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
